97483229ae22ba9c9217d2b4fc6864c7336291be
John Wiseman <john.wiseman@cantab.net>
2026-09-23 16:04:04 +0100

    6.0.25.41
    (cherry picked from commit 4b7a47b62b4411e59a8c1ade62b2017c4e7cc93c)

--- TelnetV6.c
+++ TelnetV6.c
@@ -533,6 +533,9 @@ int ProcessLine(char * buf, int Port)
 	else if (_stricmp(param,"SecureTelnet") == 0)
 		TCP->SecureTelnet = atoi(value);
 
+	else if (_stricmp(param,"noWebSocks") == 0)
+		TCP->noWebSocks = atoi(value);
+
 	else if (_stricmp(param,"CloseOnDisconnect") == 0)
 		TCP->DisconnectOnClose = atoi(value);
 
@@ -547,6 +550,8 @@ int ProcessLine(char * buf, int Port)
 	
 	else if (_stricmp(param,"HTTPPORT") == 0)
 		HTTPPort = TCP->HTTPPort = atoi(value);
+
+
 
 	else if (_stricmp(param,"NETROMPORT") == 0)
 		TCP->NETROMPort = atoi(value);
--- WebMail.c
+++ WebMail.c
@@ -1057,7 +1057,7 @@ int ViewWebMailMessage(struct HTTPConnectionInfo * Session, char * Reply, int Nu
 	if (!CheckUserMsg(Msg, User->Call, User->flags & F_SYSOP))
 	{
 		ptr += sprintf(ptr, "Message %d not for you\r", Number);
-		return sprintf(Reply, WebMailTemplate, BBSName, User->Call, Key, Key, Key, Key, Key, Key, Key, Message);
+		return sprintf(Reply, WebMailTemplate, BBSName, User->Call, Key, Key, Key, Key, Key, Key, Key, Key, Key, Key, Message);
 	}
 
 	if (_stricmp(Msg->to, "RMS") == 0)
